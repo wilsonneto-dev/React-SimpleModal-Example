2e1b4dfb849e405b0feab7ace4b6a7d789862006
Wilson Neto B.R <contato@wilsonneto.com.br>
2023-03-13 19:55:06 -0300

    simple popup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+export const App = () => {
+  const [books, _] = useState(fakeData);
+  const [selectedBook, setSelectedBook] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul style={{ 'display': !selectedBook ? 'block' : 'none' }}>
+        { 
+          books.map(b => 
+            <li key={b.code}>
+              {b.title} 
+              &nbsp;&nbsp;
+              <button onClick={() => setSelectedBook(b)}>open</button>
+            </li>
+          ) 
+        }
+      </ul>
+      { !!selectedBook && <PopUp {...selectedBook} closeCallback={() => setSelectedBook(null)} /> }
     </div>
   );
 }
 
-export default App;
+const PopUp = ({ title, description, code, closeCallback }) => {
+  return (
+      <fieldset>
+          <legend>{ title }</legend>
+          <label>Title: </label> { title }
+          <br />
+          <label>Description: </label> { description }
+          <br />
+          <label>Code: </label> { code }
+          <br />
+          
+          <br />
+          <button onClick={() => closeCallback()}>Close</button>
+      </fieldset>
+  );
+}
+
+const fakeData = [
+  { code: 2, title: "Clean Code", description: "A Handbook of Agile Software Craftsmanship" },
+  { code: 3, title: "The Pragmatic Programmer", description: "From Journeyman to Master" },
+  { code: 4, title: "Design Patterns", description: "Elements of Reusable Object-Oriented Software" },
+  { code: 5, title: "Code Complete", description: "A Practical Handbook of Software Construction" },
+  { code: 6, title: "The Mythical Man-Month", description: "Essays on Software Engineering" }
+];
